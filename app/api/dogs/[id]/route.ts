2ad1554e2a1668550ac0978f73addb8f51188194
prxoio <86233425+prxoio@users.noreply.github.com>
2024-03-30 12:10:48 +0000

    Refactor dog data fetching and formatting in route.ts

--- app/api/dogs/[id]/route.ts
+++ app/api/dogs/[id]/route.ts
@@ -3,25 +3,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import parseCsv from 'lib/parseCsv';
 import type { Dog } from 'types/dog';
 
-let cache: Dog[] | null = null; // Initialize cache
+let cache: Dog[] | null = null;
 
 async function fetchDogData(dogId: string): Promise<Dog | null> {
-  // Load data into cache if it's not already loaded
   if (cache === null) {
     console.log('Loading CSV data into cache...');
-    cache = await parseCsv('data/dogs.csv'); // Correct the path as needed
+    cache = await parseCsv('./public/data/dogs.csv'); // Make sure the path is correct
   }
-  
-  // Replace dashes with spaces in the dogId
-  const formattedDogId = dogId.replace(/-/g, ' ');
 
-  // Find the dog with the formatted ID
-  return cache.find(dog => dog.id === formattedDogId) || null;
+  const formattedDogId = dogId.replace(/-/g, ' ');
+  return cache.find((dog) => dog.id === formattedDogId) || null;
 }
 
-export async function GET(request: NextRequest) {
-  const dogId = request.nextUrl.pathname.split('/').pop();
-  const dogData = await fetchDogData(dogId!);
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const dogId = request.nextUrl.pathname.split('/').pop() ?? '';
+
+  const dogData = await fetchDogData(dogId);
 
   if (dogData) {
     return new NextResponse(JSON.stringify(dogData), {
