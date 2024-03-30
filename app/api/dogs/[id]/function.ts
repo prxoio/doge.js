4762c387301e3ea8da37665c992ce1ef23cfc8b0
prxoio <86233425+prxoio@users.noreply.github.com>
2024-03-30 12:27:33 +0000

    Refactor fetchDogData function to improve performance

--- app/api/dogs/[id]/function.ts
+++ app/api/dogs/[id]/function.ts
@@ -1,14 +1,10 @@
-// app/api/dogs/[id]/route.ts
-import parseCsv from 'lib/parseCsv';
-import type { Dog } from 'types/dog';
-let cache: Dog[] | null = null;
+import parseCsv from "lib/parseCsv";
+import type { Dog } from "types/dog";
+
 export async function fetchDogData(dogId: string): Promise<Dog | null> {
-    if (cache === null) {
-      console.log('Loading CSV data into cache...');
-      cache = await parseCsv('data/dogs.csv'); // Make sure the path is correct
-    }
-  
-    const formattedDogId = dogId.replace(/-/g, ' ');
-    return cache.find((dog) => dog.id === formattedDogId) || null;
-  }
-  
+  console.log("Parsing CSV data for every request...");
+  const dogs: Dog[] = await parseCsv("data/dogs.csv"); // Make sure the path is correct
+
+  const formattedDogId = dogId.replace(/-/g, " ");
+  return dogs.find((dog) => dog.id === formattedDogId) || null;
+}
